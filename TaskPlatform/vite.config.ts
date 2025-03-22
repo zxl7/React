3049bf9f03fd00362f1c141811e20ec2ee668ada
xanadu.zxl <xanadu.zxl@gmail.com>
2025-03-22 18:48:59 +0800

    feat: 初始化H5项目模板，集成Antd Mobile、React Router、Zustand等库

--- TaskPlatform/vite.config.ts
+++ TaskPlatform/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "~": "/src",
+    },
+  },
 })
